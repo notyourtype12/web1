b35e9dd5187832bbae0ff9bb588e5b8f2e76a780
notyourtype12 <tytydak@gmail.com>
2024-09-02 09:39:58 +0700

    Inisialisasi Commit

---
+++ Praktikum1.php
@@ -0,0 +1,10 @@
+<?php
+$nim = "E41230429";
+$name = "Budi";
+$prodi = "Teknik";
+echo "Selamat praktikum" . PHP_EOL;
+echo $nim;
+echo $name;
+echo $prodi;
+
+?>
---
+++ operator1.php
@@ -0,0 +1,11 @@
+<?php
+$tugas1 = 90;
+$tugas2 = 90;
+$jumlah = $tugas1 + $tugas2;
+$rata = $jumlah/2;
+echo "Nilai tugas 1 :".$tugas1. "<br>";
+echo "Nilai tugas 2 :".$tugas2. "<br>";
+echo "Jumlah Tugas :".$jumlah. "<br>";
+echo "Rerata Tugas : ".$rata;
+?>
+
